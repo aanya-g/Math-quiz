e1a7c80322c4a85878a5824119224144b82863ba
aanya-g <73871099+aanya-g@users.noreply.github.com>
2021-07-07 17:46:12 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,8 +1,10 @@
 var player1_name=localStorage.getItem("player1_name");
 var player2_name=localStorage.getItem("player2_name");
+player1_score = 0;
+player2_score = 0;
 
-document.getElementById("player1_name").innerHTML=player1_name;
-document.getElementById("player2_name").innerHTML=player2_name;
+document.getElementById("player1_name").innerHTML=player1_name+"=";
+document.getElementById("player2_name").innerHTML=player2_name+"=";
 
 
 function send(){
@@ -18,4 +20,41 @@ function send(){
     document.getElementById("no1").value="";
     document.getElementById("no2").value="";
 
+}
+question_turn = "player_1";
+answer_turn = "player_2";
+
+function check(){
+  get_answer = document.getElementById("input_check_box").value;
+  if (get_answer == actual_answer) {
+      if (answer_turn == "player_2"){
+          player2_score= player2_score + 1;
+          document.getElementById("player2_score").innerHTML = player2_score;
+      }
+      else{
+          player1_score = player1_score + 1;
+          document.getElementById("player1_score").innerHTML = player1_score;
+      }
+  }
+
+  if (question_turn == "player_1") {
+
+    console.log("qt-"+question_turn+"   --- at- "+answer_turn);
+      question_turn="player_2";
+      answer_turn = "player_1";
+      console.log("qt-"+question_turn+"   --- at- "+answer_turn);
+      document.getElementById("player_question").innerHTML = "Question turn -" + player2_name;
+     
+      document.getElementById("player_answer").innerHTML = "Answer turn -" + player1_name;
+  }
+  else{
+    console.log("elseqt-"+question_turn+"   --- at- "+answer_turn);
+    question_turn="player_1";
+    answer_turn = "player_2";
+    console.log("qt-"+question_turn+"   --- at- "+answer_turn);
+      document.getElementById("player_question").innerHTML = "Question turn -" + player1_name;
+      
+      document.getElementById("player_answer").innerHTML = "Answer turn -" + player2_name;
+   }
+   document.getElementById("final").innerHTML = "";
 }
